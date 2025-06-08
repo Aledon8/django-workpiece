4b47bfb937196ee23d9ac3ac28ffd2494a7e78a6
Donich Alexander <Sasha.uz.ru@gmail.com>
2025-06-08 14:22:12 +0500

    feat(api): add jwt authentication and swagger documentation
    - Implement JWT token obtain and refresh endpoints
    - Add Swagger UI and ReDoc for API documentation
    - Configure media URL handling for development

--- workpiece/workpiece/urls.py
+++ workpiece/workpiece/urls.py
@@ -14,9 +14,32 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import include, path, re_path
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework import permissions
+# Подключение Swagger для документации
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Workpiece API",
+        default_version='v1',
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    
+     # JWT Login и Refresh
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    
+    re_path(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    re_path(r"^swagger/$", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    re_path(r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
